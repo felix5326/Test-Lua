bc9b11a075dbb283304f9387bb8188e985072c47
canmera <81864321+rm1dev@users.noreply.github.com>
2021-12-17 21:29:37 +0300

    Update script.lua

--- script.lua
+++ script.lua
@@ -2,23 +2,23 @@ local value = {}
 local config = gg.EXT_CACHE_DIR.."/"..
 gg.getFile():match("").. ".cfg"
 local data = loadfile(config)
-if data ~= nil then value = data() data = nil end id = "nickname"pass = "pass"while(true)do value = gg.prompt({"Kullanici Adi","Sifre"},value,{"text","text"})
+if data ~= nil then value = data() data = nil end id = "nickname"pass = "pass"while(true)do value = gg.prompt({"Name","Password"},value,{"text","text"})
 if value == nil 
-then gg.toast("Hic bir sey secmedin")
+then gg.toast("empty")
 return
 end 
 if value[3]
  then gg.saveVariable(value, config)os.remove(config)
 else
 end
-if id == value[1] and pass == value[2] then gg.alert("Giris Basarili")
+if id == value[1] and pass == value[2] then gg.alert("Successful login")
 break
 else 
-if id ~= value[1] and pass == value[2] then gg.alert("Nickname hatalı")
+if id ~= value[1] and pass == value[2] then gg.alert("Error: Nickname")
 else if 
-id == value[1] and pass ~= value[2] then gg.alert("Pass hatalı")
+id == value[1] and pass ~= value[2] then gg.alert("Error: Password")
 else 
-gg.alert("Ikiside hatali")
+gg.alert("Error: Nickname, Password")
 end end end end 
 
-choice = gg.choice({"Fov", "Aimbot","Exit"})
+choice = gg.choice({"CHOOSE1", "CHOOSE2","Exit"})
